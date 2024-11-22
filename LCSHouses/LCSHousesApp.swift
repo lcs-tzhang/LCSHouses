90478d77e97477c9e2a61469dee6e453223ff000
Russell Gordon <rgordon@lcs.on.ca>
2024-11-21 21:00:50 -0500

    Initial Commit

---
+++ LCSHouses/LCSHousesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSHousesApp.swift
+//  LCSHouses
+//
+//  Created by Russell Gordon on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct LCSHousesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
